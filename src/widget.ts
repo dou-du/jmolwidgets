4507ffa0fad91d8ba7efe07b7a404ccb844c3f57
Dou Du <dou.du.epfl@gmail.com>
2020-02-11 15:41:25 +0100

    Use the JmolRadioGroup Button

--- src/widget.ts
+++ src/widget.ts
@@ -33,15 +33,15 @@ class JmolModel extends DOMWidgetModel {
   }
 
   initialize() {
-        DOMWidgetModel.prototype.initialize.apply(this, arguments);
-        this.attributes['jmol_window_id'] = _.uniqueId('jmol_window');
-        this.attributes['jmol_app_id'] = _.uniqueId('jmol_app');
-    }
+    DOMWidgetModel.prototype.initialize.apply(this, arguments);
+    this.attributes['jmol_window_id'] = _.uniqueId('jmol_window');
+    this.attributes['jmol_app_id'] = _.uniqueId('jmol_app');
+  }
 
   static serializers: ISerializers = {
-      ...DOMWidgetModel.serializers,
-      // Add any extra serializers here
-    }
+    ...DOMWidgetModel.serializers,
+    // Add any extra serializers here
+  }
 
   static model_name = 'JmolModel';
   static model_module = MODULE_NAME;
@@ -54,18 +54,18 @@ class JmolModel extends DOMWidgetModel {
 
 export
 class JmolView extends DOMWidgetView {
-    template = _.template("<div id='jsmolapp' style='border:3px solid red; height: " + 500 +
-                          "px; width: " + 500 + "px; margin:0 auto;'>");
-                          
-    _jmolradio: HTMLElement = document.createElement('div');
+  template = _.template("<div id='jsmolapp' style='border:3px solid red; height: " + 500 +
+  "px; width: " + 500 + "px; margin:0 auto;'>");
 
-    createDiv() {
-        const jsmolwindowID = this.model.get('jmol_window_id');
-        console.log(jsmolwindowID+"&*&*&*&*&*&*");
-        const divstyle = $("<div id='"+ jsmolwindowID + "' style='border:3px solid red; height: " +
-                             500 + "px; width: " + 500 + "px; margin:0 auto;'>");
-        return(divstyle);
-    }
+  _jmolradio: HTMLElement = document.createElement('div');
+
+  createDiv() {
+    const jsmolwindowID = this.model.get('jmol_window_id');
+    console.log(jsmolwindowID+"&*&*&*&*&*&*");
+    const divstyle = $("<div id='"+ jsmolwindowID + "' style='border:3px solid red; height: " +
+    500 + "px; width: " + 500 + "px; margin:0 auto;'>");
+    return(divstyle);
+  }
 
   initialize(): void {
 
@@ -77,67 +77,68 @@ class JmolView extends DOMWidgetView {
     document.querySelector("head")!.appendChild(script);
   }
 
-    createView() {
-        const jsmolwindowID = this.model.get('jmol_window_id');
-        const jsmolappID = this.model.get('jmol_app_id');
+  createView() {
+    const jsmolwindowID = this.model.get('jmol_window_id');
+    const jsmolappID = this.model.get('jmol_app_id');
 
-        var that = this;
-        (<any>window).set_measure = (a: any, b: any, c: any, d: any, e: any)=>{
-            console.log("The distance is:" + e);
-            that.model.set('measure_distance', Number(e));
-            that.touch();
-        };
+    var that = this;
+    (<any>window).set_measure = (a: any, b: any, c: any, d: any, e: any)=>{
+      console.log("The distance is:" + e);
+      that.model.set('measure_distance', Number(e));
+      that.touch();
+    };
 
-        (<any>window).set_pickcallback = (a: any, b: any, c: any)=>{
-            console.log("The picked atomno:" + c);
-            that.model.set('atomno', Number(c));
-            that.touch();
-        };
+    (<any>window).set_pickcallback = (a: any, b: any, c: any)=>{
+      console.log("The picked atomno:" + c);
+      that.model.set('atomno', Number(c));
+      that.touch();
+    };
 
-        $(document).ready(function() {
-            var Info = {
-                addSelectionOptions: false,
-                debug: false,
-                j2sPath: "https://chemapps.stolaf.edu/jmol/jsmol/j2s",
-                readyFunction: null,
-                script: "background black; frank on; cartoon on; spacefill off; wireframe off; backbone 1.5; color backbone chains",
-                serverURL: "https://chemapps.stolaf.edu/jmol/jsmol/php/jsmol.php",
-                src: null,
-                use: "HTML5",
-                height:"100%",
-                width:"100%",
-                MeasureCallback: "set_measure",
-             };
+    $(document).ready(function() {
+      var Info = {
+        addSelectionOptions: false,
+        debug: false,
+        j2sPath: "https://chemapps.stolaf.edu/jmol/jsmol/j2s",
+        readyFunction: null,
+        script: "background black; frank on; cartoon on; spacefill off; wireframe off; backbone 1.5; color backbone chains",
+        serverURL: "https://chemapps.stolaf.edu/jmol/jsmol/php/jsmol.php",
+        src: null,
+        use: "HTML5",
+        height:"100%",
+        width:"100%",
+        MeasureCallback: "set_measure",
+      };
 
-        $("#"+jsmolwindowID).html(Jmol.getAppletHtml(jsmolappID, Info));
-        $("#"+jsmolwindowID).mouseleave(() => {
-          let orientation:string = Jmol.scriptEcho(eval(jsmolappID), "show orientation");
-          that.model.set('_current_orientation', orientation);
-          that.touch();
-        });
-        Jmol.script(eval(jsmolappID), "load https://files.rcsb.org/view/1zaa.pdb;");
-        Jmol.script(eval(jsmolappID), 'set pickCallback "set_pickcallback"');
+      $("#"+jsmolwindowID).html(Jmol.getAppletHtml(jsmolappID, Info));
+      $("#"+jsmolwindowID).mouseleave(() => {
+        let orientation:string = Jmol.scriptEcho(eval(jsmolappID), "show orientation");
+        that.model.set('_current_orientation', orientation);
+        that.touch();
+      });
+      Jmol.script(eval(jsmolappID), "load https://files.rcsb.org/view/1zaa.pdb;");
+      Jmol.script(eval(jsmolappID), 'set pickCallback "set_pickcallback"');
 
-        that._jmolradio.innerHTML = Jmol.jmolRadio(eval(jsmolappID), 'spin on');
+      let jscript = ['spin on', 'spin off'];
+      that._jmolradio.innerHTML = Jmol.jmolRadioGroup(eval(jsmolappID),  jscript);
     });
 
-    }
+  }
 
-    render() {
-      //  this.el.classList.add('custom-widget');
-      //  this.$el.html(this.template);
-      this.$el.append(this.createDiv());
-      this.createView();
+  render() {
+    //  this.el.classList.add('custom-widget');
+    //  this.$el.html(this.template);
+    this.$el.append(this.createDiv());
+    this.createView();
 
-      const hr: HTMLElement = document.createElement('hr');
-      this.$el.append(hr);
-      this.$el.append(this._jmolradio);
+    const hr: HTMLElement = document.createElement('hr');
+    this.$el.append(hr);
+    this.$el.append(this._jmolradio);
 
-      this.model.on('change:script', this._script_changed, this);
-      this.model.on('change:structure', this._structure_changed, this);
+    this.model.on('change:script', this._script_changed, this);
+    this.model.on('change:structure', this._structure_changed, this);
 
-      //  this.model.on('change:value', this._value_changed, this);
-    }
+    //  this.model.on('change:value', this._value_changed, this);
+  }
 
   private _script_changed(){
     const jsmolappID = this.model.get('jmol_app_id');
@@ -153,7 +154,7 @@ class JmolView extends DOMWidgetView {
     Jmol.script(eval(jsmolappID), the_script);
   }
 
-//   _value_changed() {
-//       this.el.textContent = this.model.get('value');
-//   }
+  //   _value_changed() {
+  //       this.el.textContent = this.model.get('value');
+  //   }
 }
